ab3ecd1106ed9bf906798d25a87198b6f211243d
devjyoti-nobroker <dev.jyoti@nobroker.in>
2025-05-29 14:19:05 +0530

    init

---
+++ src/main/java/com/nobroker/streamSphere/StreamSphereApplication.java
@@ -0,0 +1,13 @@
+package com.nobroker.streamSphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamSphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamSphereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nobroker/streamSphere/StreamSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nobroker.streamSphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamSphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
