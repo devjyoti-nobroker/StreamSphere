51c9aa362e2e6f1a666444123acf0d55af27df9b
priyanshu <priyanshuraj30122003@gmail.com>
2025-05-30 13:23:57 +0530

    Repositories Added

---
+++ src/main/java/com/nobroker/streamSphere/repo/WatchHistoryRepository.java
@@ -0,0 +1,21 @@
+package com.nobroker.streamSphere.repo;
+
+import com.nobroker.streamSphere.models.WatchHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface WatchHistoryRepository extends JpaRepository<WatchHistory, Long> {
+
+    //Finds all watch history entries for a particular profile in descending order of watchedAt timestamp
+    List<WatchHistory> findByProfileIdOrderByWatchedAtDesc(Long profileId);
+
+    //Find if a movie was already watched by a profile
+    boolean existsByProfileIdAndMovieId(Long profileId, Long movieId);
+
+    //Delete watch history for a specific movie of a profile
+    void deleteByProfileIdAndMovieId(Long profileId, Long movieId);
+
+}
