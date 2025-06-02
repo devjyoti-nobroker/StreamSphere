c6addfe8b504f4872fcd077b8fe9fa4be8eea8e9
mayank <subham.bhakat01@gmail.com>
2025-06-02 11:33:34 +0530

    Script updated with dummy data for users,movies and genres.

--- scripts/create_mysql_tables.sql
+++ scripts/create_mysql_tables.sql
@@ -2,36 +2,39 @@ CREATE DATABASE  IF NOT EXISTS `stream_sphere`;
 USE `stream_sphere`;
 
 -- Disable foreign key checks
--- SET FOREIGN_KEY_CHECKS = 0;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- Truncate all tables
--- DROP TABLE watch_history;
--- DROP TABLE watch_list;
--- DROP TABLE movie_actors;
--- DROP TABLE movie_genre; 
--- DROP TABLE user_profile;
--- DROP TABLE movies;
--- DROP TABLE actors;
--- DROP TABLE genre;
--- DROP TABLE profiles;
--- DROP TABLE users;
+DROP TABLE IF EXISTS watch_history;
+DROP TABLE IF EXISTS watch_list;
+DROP TABLE IF EXISTS movie_genre; 
+DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS profiles;
+DROP TABLE IF EXISTS accounts;
+DROP TABLE IF EXISTS home_config;
+
 
 -- Re-enable foreign key checks
--- SET FOREIGN_KEY_CHECKS = 1;
+SET FOREIGN_KEY_CHECKS = 1;
 
-CREATE TABLE users (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE accounts (
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(100),
-    password_hash VARCHAR(255)
+    password_hash VARCHAR(255),
+    active TINYINT(1),
+    created DATETIME,
+    last_updated DATETIME
 );
 
 CREATE TABLE profiles (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     adult BOOLEAN,
-    user_id BIGINT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    account_id BIGINT NOT NULL,
+    created DATETIME,
+    last_updated DATETIME,
+    FOREIGN KEY (account_id) REFERENCES accounts(id)
 );
 
 CREATE TABLE movies (
@@ -42,14 +45,24 @@ CREATE TABLE movies (
     description VARCHAR(255),
     rating FLOAT,
     poster VARCHAR(255),
-    genre VARCHAR(255),
-    actors VARCHAR(255)
+    actors VARCHAR(255),
+    createdAt DATE,
+    updatedAt DATE,
+	updatedBy BIGINT,
+    FOREIGN KEY (updatedBy) REFERENCES accounts(id)
+);
+
+CREATE TABLE movie_genre (
+    movie_id BIGINT NOT NULL,
+    genre VARCHAR(255) NOT NULL,
+    PRIMARY KEY (movie_id, genre),
+    FOREIGN KEY (movie_id) REFERENCES movies(id)
 );
 
 CREATE TABLE watch_list (
     profile_id BIGINT NOT NULL,
     movie_id BIGINT NOT NULL,
-    time_stamp DATE,
+    time_stamp DATETIME,
     PRIMARY KEY (profile_id, movie_id),
     FOREIGN KEY (movie_id) REFERENCES movies(id),
     FOREIGN KEY (profile_id) REFERENCES profiles(id)
@@ -58,7 +71,7 @@ CREATE TABLE watch_list (
 CREATE TABLE watch_history (
     profile_id BIGINT NOT NULL,
     movie_id BIGINT NOT NULL,
-    time_stamp DATE,
+    time_stamp DATETIME,
     PRIMARY KEY (profile_id, movie_id),
     FOREIGN KEY (movie_id) REFERENCES movies(id),
     FOREIGN KEY (profile_id) REFERENCES profiles(id)
@@ -68,3 +81,12 @@ CREATE TABLE home_config (
     id INT AUTO_INCREMENT PRIMARY KEY,
     value INT NOT NULL UNIQUE
 );
+
+INSERT INTO accounts (email, name, password_hash, active, created, last_updated)
+VALUES
+('john.doe@example.com', 'John Doe', 'hashed_pw_1', 1, NOW(), NOW()),
+('jane.smith@example.com', 'Jane Smith', 'hashed_pw_2', 1, NOW(), NOW()),
+('alice.wong@example.com', 'Alice Wong', 'hashed_pw_3', 0, NOW(), NOW());
+
+
+SELECT * FROM accounts;
