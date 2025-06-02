b1dcc14b9e3f19361fcff36b3ceceebb166b2031
mayank <subham.bhakat01@gmail.com>
2025-06-02 12:45:05 +0530

    Everything releated to movies is pushed.Except no controoler made for delete movies.And I have to make some changes in post/put movies especially for images.

---
+++ src/main/java/com/nobroker/streamSphere/contollers/MoviesController.java
@@ -0,0 +1,159 @@
+package com.nobroker.streamSphere.contollers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nobroker.streamSphere.models.Movies;
+import com.nobroker.streamSphere.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
+public class MoviesController {
+
+    @Autowired
+    private MovieService movieService;
+
+
+
+    @GetMapping("/movies")
+    public ResponseEntity<?> getMovies() {
+        try {
+            // Ok
+            List<Movies> movies = movieService.getAllMovies();
+            return ResponseEntity.ok(movies);
+
+        } catch (Exception e) {
+            // Log the error if needed
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Failed to fetch movies: " + e.getMessage());
+        }
+    }
+
+
+
+    @GetMapping("/movies/{id}")
+    public ResponseEntity<?> getMovieById(@PathVariable Long id) {
+        try {
+            Optional<Movies> movie = movieService.getMovieById(id);
+            if (movie.isPresent()) {
+                return ResponseEntity.ok(movie.get());
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                        .body("Movie with ID " + id + " not found.");
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error fetching movie: " + e.getMessage());
+        }
+    }
+
+
+
+    @GetMapping("/movies/sort/releaseDate/asc")
+    public ResponseEntity<?> getMoviesSortedByReleaseDateAsc() {
+        try {
+            List<Movies> movies = movieService.getMoviesSortedByReleaseDateAsc();
+            return ResponseEntity.ok(movies);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error fetching movies: " + e.getMessage());
+        }
+    }
+
+
+
+    @GetMapping("/movies/sort/releaseDate/desc")
+    public ResponseEntity<?> getMoviesSortedByReleaseDateDesc() {
+        try {
+            List<Movies> movies = movieService.getMoviesSortedByReleaseDateDesc();
+            return ResponseEntity.ok(movies);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error fetching movies: " + e.getMessage());
+        }
+    }
+
+    @GetMapping("/movies/sort/rating/asc")
+    public ResponseEntity<?> getMoviesSortedByRatingAsc() {
+        try{
+            List<Movies> movies = movieService.getMoviesSortedByRatingAsc();
+            return ResponseEntity.ok(movies);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error fetching movies: " + e.getMessage());
+        }
+    }
+
+
+    @GetMapping("/movies/sort/rating/desc")
+    public ResponseEntity<?>getMoviesSortedByRatingDesc() {
+        try {
+            List <Movies> movies = movieService.getMoviesSortedByRatingDesc();
+            return ResponseEntity.ok(movies);
+        }catch(Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error fetching movies: " + e.getMessage());
+        }
+    }
+
+
+
+
+    @PostMapping(value = "/movies", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<?> addMovie(@RequestPart("movie") String movieJson,
+                                      @RequestPart("imageFile") MultipartFile imageFile) {
+        try {
+            // Manually convert JSON string to Movies object
+            ObjectMapper objectMapper = new ObjectMapper();
+            Movies movie = objectMapper.readValue(movieJson, Movies.class);
+
+            Movies savedMovie = movieService.addMovie(movie, imageFile);
+            return ResponseEntity.ok(savedMovie);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error saving movie: " + e.getMessage());
+        }
+    }
+
+
+
+    @PutMapping(value = "/movies/{id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<?> updateMovie(@PathVariable Long id,
+                                         @RequestPart("movie") String movieJson,
+                                         @RequestPart(value = "imageFile", required = false) MultipartFile imageFile) {
+        try {
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            Movies movie = objectMapper.readValue(movieJson, Movies.class);
+
+            Movies updatedMovie = movieService.updateMovie(id, movie, imageFile);
+
+            return ResponseEntity.ok(updatedMovie);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error updating movie: " + e.getMessage());
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/main/java/com/nobroker/streamSphere/models/Movies.java
@@ -0,0 +1,60 @@
+package com.nobroker.streamSphere.models;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="movies")
+public class Movies {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long movieId;
+
+    @Column(name="title")
+    private String movieName;
+
+    @Column(name="released")
+    private Date releaseDate;
+
+    @Column(name="runtime")
+    private String runTime;
+
+    @Column(name="description")
+    private String description;
+
+    @Column(name="rating")
+    private float rating;
+
+    @Column(name="actors")
+    private String actorList;
+
+    @Column(name="image")
+    private String moviePoster;
+
+    @Column(name="image_type")
+    private String imageType;
+
+    @Lob
+    @Column(name="image_data")
+    private byte[] imageData;
+
+    @Column(name="created_at")
+    private Date createdAt;
+
+    @Column(name="updated_at")
+    private Date updatedAt;
+
+    @Column(name="updated_by")
+    private Long updatedBy;
+
+}
