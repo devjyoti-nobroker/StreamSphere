40258f06514f9c1075f3e3ab3c62037da27678af
devjyoti-nobroker <dev.jyoti@nobroker.in>
2025-06-05 15:43:38 +0530

    added genre in getMovieById

--- src/main/java/com/nobroker/streamSphere/config/SecurityConfig.java
+++ src/main/java/com/nobroker/streamSphere/config/SecurityConfig.java
@@ -20,6 +20,8 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
@@ -50,6 +52,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable) // ✅ new syntax
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.POST, "/api/register","/auth/login").permitAll()
@@ -70,6 +73,17 @@ public class SecurityConfig {
         return config.getAuthenticationManager();
     }
 
-
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")       // Allow all origins
+                        .allowedMethods("*")       // Allow all methods
+                        .allowedHeaders("*");      // Allow all headers
+            }
+        };
+    }
 
 }
