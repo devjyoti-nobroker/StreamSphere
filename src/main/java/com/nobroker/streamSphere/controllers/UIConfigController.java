03caff8706e5a7bc08d7f7c2ba7b97559ac255c4
mayank <subham.bhakat01@gmail.com>
2025-06-11 12:38:17 +0530

    Dail commit streak

--- src/main/java/com/nobroker/streamSphere/controllers/UIConfigController.java
+++ src/main/java/com/nobroker/streamSphere/controllers/UIConfigController.java
@@ -1,4 +1,20 @@
 package com.nobroker.streamSphere.controllers;
 
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+
+@RestController
+@RequestMapping("/api/ui-config")
 public class UIConfigController {
+
+    @GetMapping
+    public ResponseEntity<String> getUIConfig() throws IOException {
+        ClassPathResource resource = new ClassPathResource("config/ui-config.json");
+        String json = Files.readString(resource.getFile().toPath(), StandardCharsets.UTF_8);
+        return ResponseEntity.ok(json);
+    }
 }
